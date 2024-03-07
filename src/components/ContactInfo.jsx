531c76f1c51542b280ad3a7b2ce6c2b3a5ad0ddd
rezahaddad098 <haddadreza098@gmail.com>
2024-03-07 11:24:30 +0330

    full project

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function ContactInfo() {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h2 className="fs-1 mb-3 text-uppercase fw-bold">Where To Find Us</h2>
+            <p className="mb-5">Saadat Abad</p>
+            <h3>Opening Hours</h3>
+            <p className="m-0">Shnabe - Panjshanbe: 09:00 - 17:00</p>
+            <p>Jome: 10:00 - 15:00</p>
+        </div>
+    )
+}
